48b65313e552cedd7557e8e1804b68821ebe9fe8
PARKNAYEON <parkna96@gmail.com>
2021-03-13 16:32:34 +0900

    update

---
+++ code_merge/ClassifyWord.py
@@ -0,0 +1,40 @@
+from matplotlib import font_manager
+import csv
+import os
+
+#긍정, 부정, 중립 사전 분류
+font_path = 'C:/Users/parkn/Desktop/맑은고딕/malgun.ttf'
+
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+
+os.chdir("/Users/parkn/Desktop/삼성전자2/")
+
+
+
+
+def write_data(data, filename, encoding='UTF8'):
+    with open(filename, 'w', encoding=encoding) as f:
+        f.write(data)
+
+
+def classifyword():
+    positive_data = ''
+    negative_data = ''
+    neu_data = ''
+    count = 1
+    with open('전체형태소_삼성전자.csv','r',encoding='UTF8') as f:
+        reader = csv.reader(f)
+
+        for a in reader:
+            if a[2] == '-1':
+                negative_data += a[0] + "\n"
+            elif a[2] == '1':
+                positive_data += a[0] + "\n"
+            else:
+                neu_data += a[0] + "\n"
+
+            count += 1
+
+    write_data(positive_data,'긍정_전체형태소_감성사전.txt', encoding='UTF8')
+    write_data(negative_data, '부정_전체형태소_감성사전.txt', encoding='UTF8')
+    write_data(neu_data,'중립_감성사전.txt', encoding='UTF8')
